8a0ae821810adf0a0a9656dd0ef1fe97354d897d
2clee8 <cynthia_lee28@milton.edu>
2025-11-07 08:38:47 -0500

    added constructor to factory

--- src/Factory.java
+++ src/Factory.java
@@ -1,5 +1,34 @@
 public class Factory {
+
+//need to put *public before *int if used before methods
+
+    // int variable for the year factory was founded
+    public int yearFounded = 1798;
+    public String factoryName = "airplane Factory";
+    public boolean isOpen = true;
+        //psvm
     public static void main(String[] args) {
+        //sout
         System.out.println("welcome to Cynthia's factory");
+        //saying we want to call the constructor "Factory" and make a new one
+        new Factory();
+    }
+
+    //constructor: very special type of method, has exact same name as class,also because doens't have void
+    public Factory(){
+        isOpen=false;
+        //scope for manager is only in the constructor
+        String manager = "Mr. Hales";
+        System.out.println(manager);
+        factoryInfo();
+
+        //todo right here
+    }
+
+    //empty method called factoryInfo
+    public void factoryInfo(){
+        System.out.println(factoryName);
+        System.out.println(isOpen);
+        System.out.println("year founded " + yearFounded);
     }
 }
