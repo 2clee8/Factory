7bf88eb68b99112d61980e998bb026370cf214f9
2clee8 <cynthia_lee28@milton.edu>
2025-11-12 09:03:02 -0500

    added constructor to factory

---
+++ src/Airplane.java
@@ -0,0 +1,21 @@
+public class Airplane {
+    //instance variables
+    //1. used in whole class
+    //2. should be at the top of the class
+    //3. need the word public before it
+    public int capacity;
+    public String name;
+    public String model;
+    public boolean doubleDecker;
+    public int length;
+
+    //constructor
+    public Airplane(){
+        capacity = 250;
+        name = "Jacob";
+        model = "boeing 777";
+        doubleDecker = false;
+        length = 150;
+
+    }
+}
--- src/Factory.java
+++ src/Factory.java
@@ -6,15 +6,20 @@ public class Factory {
     public int yearFounded = 1798;
     public String factoryName = "airplane Factory";
     public boolean isOpen = true;
+
         //psvm
     public static void main(String[] args) {
         //sout
         System.out.println("welcome to Cynthia's factory");
         //saying we want to call the constructor "Factory" and make a new one
         new Factory();
+
     }
 
-    //constructor: very special type of method, has exact same name as class,also because doens't have void
+    //constructor
+    //1. is a method
+    //2. has same name as class
+    //3. no void
     public Factory(){
         isOpen=false;
         //scope for manager is only in the constructor
@@ -25,6 +30,19 @@ public class Factory {
         //todo right here
         yearFounded = 2024;
         factoryInfo();
+
+        //type varName = value
+        //object of type airplane
+
+        Airplane plane1 = new Airplane();
+        plane1.name = "jeff";plane1.capacity = 635;
+        plane1.doubleDecker = false;
+        plane1.length = 36;
+        plane1.model = "Boeing 777";
+        System.out.println(plane1.name);
+        Airplane plane2 = new Airplane();
+        System.out.println(plane2.capacity);
+
 
     }
 
